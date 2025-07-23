713d1d6c23ea1ae93824dcf7549c59e7800ec1dc
bobjackman-dev <nandanmd@proton.me>
2025-07-24 01:37:14 +0530

    uh

--- netlify/functions/create-user.js
+++ netlify/functions/create-user.js
@@ -1,8 +1,8 @@
 const { createClient } = require('@supabase/supabase-js');
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,        // <-- NOT VITE_
-  process.env.SUPABASE_ANON_KEY    // <-- NOT VITE_
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 exports.handler = async function(event, context) {
